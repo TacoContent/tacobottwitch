c69e28aace7da8157631fe9a17cc08213f552cc2
Ryan Conrad <camalot@gmail.com>
2022-04-21 21:19:39 -0500

    added support command

--- bot/cogs/bot_commands.py
+++ bot/cogs/bot_commands.py
@@ -38,8 +38,8 @@ class BotCommands(commands.Cog):
     async def raid(self, ctx, source_channel: str, dest_channel: str):
         _method = inspect.stack()[1][3]
         try:
-            source_channel = source_channel.lower().strip().replace("#", "").replace("@", "")
-            dest_channel = dest_channel.lower().strip().replace("#", "").replace("@", "")
+            source_channel = self.clean_channel_name(source_channel)
+            dest_channel = self.clean_channel_name(dest_channel)
 
             if not self.permissions_helper.has_permission(ctx.message.author, permissions.PermissionLevel.BOT):
                 # ONLY action if THIS account called the command
@@ -59,10 +59,39 @@ class BotCommands(commands.Cog):
 
             reason = f"raiding the channel {dest_channel}"
             await self.tacos_log.give_user_tacos(ctx.message.channel.name, source_channel, reason, give_type=tacotypes.TacoTypes.CUSTOM, amount=5)
-            # self.db.add_twitch_event(source_channel, dest_channel)
-            # await ctx.send(f"{source_channel} raided {dest_channel}.")
-            # await ctx.send(f"TombRaid TombRaid {source_channel} raided {dest_channel} 🌮🌮🌮")
         except Exception as e:
             self.log.error(ctx.message.channel.name, _method, str(e), traceback.format_exc())
+
+    @commands.command(name="support")
+    # @commands.restrict_channels(channels=["ourtacobot", "ourtaco"])
+    async def support(self, ctx, username: str, channel: str):
+        _method = inspect.stack()[1][3]
+        try:
+            username = self.clean_channel_name(username)
+            channel = self.clean_channel_name(channel)
+
+            if not self.permissions_helper.has_permission(ctx.message.author, permissions.PermissionLevel.BOT):
+                # ONLY action if THIS account called the command
+                return
+            if not self.permissions_helper.in_command_restricted_channel(ctx):
+                self.log.debug(ctx.message.channel.name, _method, f"We are not in one of the restricted channels. we are in {ctx.message.channel.name}.")
+                return
+
+            # check the source channel is a channel that is a taco channel (this should never be false)
+            if not self.db.is_taco_known_channel(username):
+                self.log.warn(ctx.message.channel.name, _method, f"Source channel {username} is not a taco channel.")
+                return
+            # check if the destination channel is a channel that is a "taco" channel
+            if not self.db.is_taco_known_channel(channel):
+                self.log.warn(ctx.message.channel.name, _method, f"Destination channel {channel} is not a taco channel.")
+                return
+
+            reason = f"supporting the channel {channel}"
+            await self.tacos_log.give_user_tacos(ctx.message.channel.name, username, reason, give_type=tacotypes.TacoTypes.CUSTOM, amount=10)
+        except Exception as e:
+            self.log.error(ctx.message.channel.name, _method, str(e), traceback.format_exc())
+
+    def clean_channel_name(self, channel: str):
+        return channel.lower().strip().replace("#", "").replace("@", "")
 def prepare(bot):
     bot.add_cog(BotCommands(bot))
