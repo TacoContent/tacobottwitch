bd4e0c5c3eeb4df89a10125c9b3f95d50bd560a8
Ryan Conrad <camalot@gmail.com>
2023-06-08 16:46:41 -0500

    add taco logging

--- bot/cogs/lib/tacotypes.py
+++ bot/cogs/lib/tacotypes.py
@@ -14,6 +14,12 @@ class TacoTypes(Enum):
     BIRTHDAY = 9
     TWITCH = 10
     STREAM = 11
+    FOOD_PHOTO = 12
+    WDYCTW = 13
+    TECH_THURSDAY = 14
+    TACO_TUESDAY = 15
+    MENTAL_MONDAY = 16
+    FIRST_MESSAGE = 17
 
     CUSTOM = 9999
 
@@ -41,8 +47,59 @@ class TacoTypes(Enum):
             return TacoTypes.TWITCH
         elif taco_type_string == "stream_count":
             return TacoTypes.STREAM
+        elif taco_type_string == "food_photo_count":
+            return TacoTypes.FOOD_PHOTO
+        elif taco_type_string == "wdyctw_count":
+            return TacoTypes.WDYCTW
+        elif taco_type_string == "tech_thursday_count":
+            return TacoTypes.TECH_THURSDAY
+        elif taco_type_string == "taco_tuesday_count":
+            return TacoTypes.TACO_TUESDAY
+        elif taco_type_string == "mental_monday_count":
+            return TacoTypes.MENTAL_MONDAY
+        elif taco_type_string == "first_message_count":
+            return TacoTypes.FIRST_MESSAGE
         else:
             return TacoTypes.CUSTOM
+
+    @staticmethod
+    def get_db_type_from_taco_type(taco_type):
+        if taco_type == TacoTypes.JOIN:
+            return "JOIN"
+        elif taco_type == TacoTypes.BOOST:
+            return "BOOST"
+        elif taco_type == TacoTypes.REACT_REWARD:
+            return "REACT_REWARD"
+        elif taco_type == TacoTypes.SUGGEST:
+            return "SUGGEST"
+        elif taco_type == TacoTypes.INVITE:
+            return "INVITE"
+        elif taco_type == TacoTypes.REACTION:
+            return "REACTION"
+        elif taco_type == TacoTypes.REPLY:
+            return "REPLY"
+        elif taco_type == TacoTypes.TQOTD:
+            return "TQOTD"
+        elif taco_type == TacoTypes.BIRTHDAY:
+            return "BIRTHDAY"
+        elif taco_type == TacoTypes.TWITCH:
+            return "TWITCH"
+        elif taco_type == TacoTypes.STREAM:
+            return "STREAM"
+        elif taco_type == TacoTypes.FOOD_PHOTO:
+            return "FOOD_PHOTO"
+        elif taco_type == TacoTypes.WDYCTW:
+            return "WDYCTW"
+        elif taco_type == TacoTypes.TECH_THURSDAY:
+            return "TECH_THURSDAY"
+        elif taco_type == TacoTypes.TACO_TUESDAY:
+            return "TACO_TUESDAY"
+        elif taco_type == TacoTypes.MENTAL_MONDAY:
+            return "MENTAL_MONDAY"
+        elif taco_type == TacoTypes.FIRST_MESSAGE:
+            return "FIRST_MESSAGE"
+        else:
+            return "CUSTOM"
 
     @staticmethod
     def get_string_from_taco_type(taco_type):
@@ -68,5 +125,18 @@ class TacoTypes(Enum):
             return "twitch_count"
         elif taco_type == TacoTypes.STREAM:
             return "stream_count"
+        elif taco_type == TacoTypes.FOOD_PHOTO:
+            return "food_photo_count"
+        elif taco_type == TacoTypes.WDYCTW:
+            return "wdyctw_count"
+        elif taco_type == TacoTypes.TECH_THURSDAY:
+            return "tech_thursday_count"
+        elif taco_type == TacoTypes.TACO_TUESDAY:
+            return "taco_tuesday_count"
+        elif taco_type == TacoTypes.MENTAL_MONDAY:
+            return "mental_monday_count"
+        elif taco_type == TacoTypes.FIRST_MESSAGE:
+            return "first_message_count"
+
         else:
             return "custom"
